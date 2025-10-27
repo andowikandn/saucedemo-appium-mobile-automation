fbe36f0f9486a5d129d1aa441f77fd495013e601
andowikandn <ahmad.wikandono@gmail.com>
2025-10-27 08:18:04 +0700

    Updated

--- pages/sidebar.py
+++ pages/sidebar.py
@@ -9,55 +9,55 @@ class SidebarMenuPage:
         self.driver = driver
         self.wait = WebDriverWait(driver, 5)
 
-    @allure.step('User tap sidebar menu')
     def tap_sidebar_menu(self):
-        self.wait.until(
-            EC.element_to_be_clickable(SidebarMenuLocator.burger_menu)).click()
+        with allure.step('User tap sidebar menu'):
+            self.wait.until(
+                EC.element_to_be_clickable(SidebarMenuLocator.burger_menu)).click()
         
-    @allure.step('Verify view sidebar menu')
     def view_sidebar_menu(self):
-        assert self.wait.until(
-            EC.presence_of_element_located(SidebarMenuLocator.all_items)).is_displayed()
-        
-    @allure.step('User tap close sidebar menu')
+        with allure.step('Verify view sidebar menu'):
+            assert self.wait.until(
+                EC.presence_of_element_located(SidebarMenuLocator.all_items)).is_displayed()
+            
     def tap_close_sidebar_menu(self):
-        self.wait.until(
-            EC.element_to_be_clickable(SidebarMenuLocator.close_burger)).click()
+        with allure.step('User tap close sidebar menu'):
+            self.wait.until(
+                EC.element_to_be_clickable(SidebarMenuLocator.close_burger)).click()
         
-    @allure.step('User tap webview menu')
     def tap_webview_menu(self):
-        self.wait.until(
-            EC.element_to_be_clickable(SidebarMenuLocator.webview_menu)).click()
-        
-    @allure.step('User input bing url: wwww.bing.com')
+        with allure.step('User tap webview menu'):
+            self.wait.until(
+                EC.element_to_be_clickable(SidebarMenuLocator.webview_menu)).click()
+            
     def webview_search_bing_url(self, search: str):
-        self.wait.until(
-            EC.presence_of_element_located(SidebarMenuLocator.webview_input_url)).send_keys(search)
-    
-    @allure.step('User tap goto site button')
+        with allure.step('User input bing url: wwww.bing.com'):
+            self.wait.until(
+                EC.presence_of_element_located(SidebarMenuLocator.webview_input_url)).send_keys(search)
+        
     def tap_goto_site_btn(self):
-        self.wait.until(
-            EC.presence_of_element_located(SidebarMenuLocator.webview_goto_site)).click()
-        
-    @allure.step('Verify bing url: www.bing.com')
+        with allure.step('User tap goto site button'):
+            self.wait.until(
+                EC.presence_of_element_located(SidebarMenuLocator.webview_goto_site)).click()
+            
     def verify_bing_url(self):
-        logo_bing = self.wait.until(
-            EC.presence_of_element_located(SidebarMenuLocator.logo_bing))
-        assert logo_bing.is_displayed()
-        return logo_bing.text
-        
+        with allure.step('Verify bing url: www.bing.com'):
+            logo_bing = self.wait.until(
+                EC.presence_of_element_located(SidebarMenuLocator.logo_bing))
+            assert logo_bing.is_displayed()
+            return logo_bing.text
+            
 class LogoutMenuPage:
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 5)
 
-    @allure.step('User tap logout menu')
     def tap_logout_menu(self):
-        self.wait.until(
-            EC.element_to_be_clickable(SidebarMenuLocator.logout_menu)).click()
+        with allure.step('User tap logout menu'):
+            self.wait.until(
+                EC.element_to_be_clickable(SidebarMenuLocator.logout_menu)).click()
         
-    @allure.step('Verify has success logout')
     def verify_logout(self):
-        assert self.wait.until(
-            EC.presence_of_element_located(LoginLocator.login_page)
-            ).is_displayed()
+        with allure.step('Verify has success logout'):
+            assert self.wait.until(
+                EC.presence_of_element_located(LoginLocator.login_page)
+                ).is_displayed()
